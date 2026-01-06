f38eb8b3d0a54184e3c9639fb61003267f7245dc
gpetrak <gkpetrak@gmail.com>
2026-01-06 21:25:22 +0200

    test

--- geonode/base/api/views.py
+++ geonode/base/api/views.py
@@ -969,7 +969,12 @@ class ResourceBaseViewSet(ApiPresetsInitializer, MultiLangViewMixin, DynamicMode
                 geonode_resource=resource,
                 input_params={"uuid": resource.uuid},
             )
-            resouce_service_dispatcher.apply_async(args=(str(_exec_request.exec_id),), expiration=30)
+            resouce_service_dispatcher.apply_async(
+                args=(str(_exec_request.exec_id),),
+                queue="geonode",        # force it to the correct queue
+                routing_key="geonode",  # optional but safe
+                expiration=30
+            )
             return Response(
                 {
                     "status": _exec_request.status,
